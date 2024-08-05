c93d82db2ff3875fa9f0cbb4602cd43bc844c743
Emmanuel <kcemmy03@gmail.com>
2024-08-05 18:34:13 +0100

    Added an import statement

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,11 +3,12 @@
 This module contains a Coroutine
 """
 import asyncio
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Asynchronously wait for a number of coroutines to complete.
 
