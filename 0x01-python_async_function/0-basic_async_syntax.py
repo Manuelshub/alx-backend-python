3b938a421e84a137eec517c913ed4698593b7714
Emmanuel <kcemmy03@gmail.com>
2024-08-05 16:58:14 +0100

    Added Documentation to the code

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,5 +7,12 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> int:
+    """
+    Coroutine that takes in an integer argument:
+    Args:
+        max_delay: int with default value 10.
+    Description:
+        This function waits for a random delay between 0 and max_delay
+    """
     delay = random.uniform(0, max_delay)
     return delay
