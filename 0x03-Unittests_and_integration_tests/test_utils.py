68683f2a693101610302015191856afff71248a1
Emmanuel <kcemmy03@gmail.com>
2024-08-26 16:51:16 +0100

    Uncommented code

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -36,31 +36,16 @@ class TestGetJson(unittest.TestCase):
     """ Test get json
     """
 
-    # @parameterized.expand([
-    #     ("http://example.com", {"payload": True}),
-    #     ("http://holberton.io", {"payload": False}),
-    # ])
-    # def test_get_json(self, test_url, test_payload):
-    #     """Test get_json function"""
-    #     with patch('utils.requests.get') as mock_get:
-    #         mock_response = Mock()
-    #         mock_response.json.return_value = test_payload
-    #         mock_get.return_value = mock_response
-    #         result = get_json(test_url)
-    #         mock_get.assert_called_once_with(test_url)
-    #         self.assertEqual(result, test_payload)
-    def test_get_json(self):
-        """Test get_json function"""
-        test_cases = [
-            ("http://example.com", {"payload": True}),
-            ("http://holberton.io", {"payload": False}),
-        ]
-
-        for test_url, test_payload in test_cases:
-            with patch('utils.requests.get') as mock_get:
-                mock_response = Mock()
-                mock_response.json.return_value = test_payload
-                mock_get.return_value = mock_response
-                result = get_json(test_url)
-                mock_get.assert_called_once_with(test_url)
-                self.assertEqual(result, test_payload)
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """Test get_json function."""
+        with patch('utils.requests.get') as mock_get:
+            mock_response = Mock()
+            mock_response.json.return_value = test_payload
+            mock_get.return_value = mock_response
+            result = get_json(test_url)
+            mock_get.assert_called_once_with(test_url)
+            self.assertEqual(result, test_payload)
